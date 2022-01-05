453134a599566861406066f7c88c807c5ee585f8
이혁수(Lee Hyuck Su) <hslee4@humaxdigital.com>
2022-01-05 13:43:59 +0900

    Function removeQuestionMarkinString has added to solve the problem about the error of counting colored cells.

---
+++ variables.js
@@ -0,0 +1,9 @@
+const MAXIMUM_EXE_TIME = 17;
+const EXE_TERM = 5;
+const UPDATE_VAL_RANGE_ROW_START = 7;
+const UPDATE_VAL_RANGE_COL_START = 8;
+var startTime = 0;
+var currentTime = 0;
+var HQcolor = "#ffff00";
+var VNcolor = "#ff9900";
+var remRun;
